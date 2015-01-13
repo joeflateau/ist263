63600a4c978945afa6fc18d26e783e8a01d33519
Joe Flateau <joef@sidearmsports.com>
2015-01-12 20:28:49 -0500

    Update poll layout

--- src/assets/scripts/poll.js
+++ src/assets/scripts/poll.js
@@ -1,3 +1,26 @@
 $.fn.poll = function(options){
-	console.log(options.id);
+	return this.each(function(){
+	        var $poll = $(this),
+	            id = $(this).prop("id"),
+	            $graph = $poll.find("svg"),
+	            graph = nv.models.discreteBarChart()
+	              .showValues(true)
+	              .x(function(d){ return d.key; })
+	              .y(function(d){ return d.value; }),
+	            $options = $poll.find("ul > li");
+
+	        socket.on('vote:' + id, function(votes){
+	            d3.select($graph[0])
+	                .datum([{
+	                  key:"",
+	                  values:votes
+	                    }])
+	                .call(graph);
+	          });
+
+	        $options.on('click', function(){
+	            socket.emit('vote', {id:id,index:$(this).index()});
+	          });
+	        
+	      });
 }
